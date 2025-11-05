d91fe9b50058760d7b0bc9cf915b86bdfd1b3d74
mradolovic <mradolovic@gmail.com>
2025-11-05 16:44:00 +0100

    add pdfs and results

--- Software/Plot/pulsar_plot_FFT.py
+++ Software/Plot/pulsar_plot_FFT.py
@@ -9,8 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define file path
-#file_path = "ragobscutfold.txt"
-file_path = "ragobscutfold_full.txt"
+file_path = "rag_obs_20251003_02_FFT_RAFFT22.txt"
 
 # split_file_path = file_path.split('_')
 #
@@ -36,15 +35,32 @@ with open(file_path, "r") as file:
             col1.append(int(parts[0]))
             col2.append(float(parts[1]))
 
+#average of power
+#average = np.average(col2)
+#col2 = col2 - average
+
+# Parameters
+Fc = 422e6      # Center frequency (Hz)
+Fs = 2.4e6      # Sample rate (Hz)
+N  = 1024       # FFT size (number of bins)
+
+# Compute frequency for each FFT bin (0 to N-1)
+freqs = [Fc - Fs/2 + (Fs/N)*k for k in range(N)]
+freqs_MHz = [f / 1e6 for f in freqs]
+
 plt.figure()
-plt.plot(col1, col2, color='red')
+#plt.plot(col1, col2, color='red')
+plt.plot(freqs_MHz, col2, color='red')
+#plt.plot(col1, col2, linestyle = '', marker='.', color='blue',label='Power')
+#plt.legend(loc='best')
 plt.xlabel("Bin number [/]")
-plt.ylabel(f"SNR [/]")
+plt.ylabel(r"Power or PSD [W or W/$\sqrt{Hz}$, uncalibrated]")
+#plt.title(f"Power vs. Bin number\n{observation_date} - {observation_number}
 #plt.title("SNR vs. Bin number - B0329+54 - Visnjan, Tican\nFirst light on 20250310\n30 min sample from 2h obs time")
-plt.title("SNR vs. Bin number - B0329+54 - Visnjan, Tican\nFirst light on 20250310\nshit signal, all of 2hrs obs")
+plt.title("FFT - B0329+54 - Visnjan, Tican\nFirst light on 20250310 - 2h obs time")
 plt.tight_layout()
-#plt.savefig("ragobscutfold_east_test.png", dpi=300)
-plt.savefig("ragobscutfold_east_test_full.png", dpi=300)
+#plt.savefig(f"pulsar_plot_{observation_date}_{observation_number}.png")
+plt.savefig("ragobscutfold_FFT_full.png", dpi=300)
 plt.show()
 
 
